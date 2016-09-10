082a9f09084ffb4244e2ffd4da66d0275e6ddb87
yaolei <yaolei@chinasofti.com>
2016-09-10 23:49:58 +0800

    fq

--- modules/mainask/java/com/yonyou/justoask/mainask/controller/ProblemController.java
+++ modules/mainask/java/com/yonyou/justoask/mainask/controller/ProblemController.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import javax.servlet.ServletRequest;
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springside.modules.web.Servlets;
 
+import com.yonyou.justoask.favorite.controller.CollectController;
 import com.yonyou.justoask.mainask.domain.Problem;
 import com.yonyou.justoask.mainask.service.ProblemService;
 import com.yonyou.justoask.mainask.util.JSoupBaiduSearcher;
@@ -22,12 +26,15 @@ import com.yonyou.justoask.mainask.util.SearchResult;
 import com.yonyou.justoask.mainask.util.Searcher;
 import com.yonyou.justoask.mainask.util.Webpage;
 
+import core.mybatis.PageParameter;
 import core.spring.RequestMappingName;
 import core.utils.jackson.JacksonUtil;
 
 @Controller
 @RequestMapping(value = "/problem")
 public class ProblemController {
+	
+	private static Logger logger = LoggerFactory.getLogger(CollectController.class);
 
 	@Autowired
 	private ProblemService problemService;
@@ -47,27 +54,46 @@ public class ProblemController {
 		
 		String keyword = request.getParameter("keyword");//获取
 		
-		//百度搜索
-		Searcher searcher = new JSoupBaiduSearcher();
-        SearchResult searchResult = searcher.search(keyword, 1);
-        List<Webpage> webpages = searchResult.getWebpages();
-        if (webpages != null) {
-        	int i = 1;
-        	askStr.append("共搜索到" + searchResult.getTotal() + "条结果，现在为您播报前" + (webpages.size()+1) + "条结果。");
-            for (Webpage webpage : webpages) {
-            	askStr.append("结果" + (i++) + "：");
-            	//askStr.append("标题：" + webpage.getTitle());
-            	//askStr.append("摘要：" + webpage.getSummary());
-            	//askStr.append("内容：" + webpage.getContent());
-            	askStr.append(webpage.getSummary());
-            }
-        }
+		logger.info("处理查询条件值，并放入Map对象中...");
+		Map<String, Object> searchParams = new HashMap<String, Object>();
+		searchParams.put("problemDesc", keyword.replace("。", ""));
+		
+		PageParameter page = new PageParameter();
+		page.setCurrentPage(1);
+		page.setPageSize(3);
+		searchParams.put("page", page);
+		
+		int conut = 0;
+		List<Problem> searchByPage = problemService.searchByPage(searchParams);
+		if(searchByPage.size() <= 0){
+			//百度搜索
+			Searcher searcher = new JSoupBaiduSearcher();
+			SearchResult searchResult = searcher.search(keyword, 1);
+			List<Webpage> webpages = searchResult.getWebpages();
+			conut = webpages.size();
+			if (webpages != null) {
+				int i = 1;
+				askStr.append("共搜索到" + searchResult.getTotal() + "条答案。");
+				for (Webpage webpage : webpages) {
+					askStr.append("答案" + (i++) + "：");
+					//askStr.append("标题：" + webpage.getTitle());
+					//askStr.append("摘要：" + webpage.getSummary());
+					//askStr.append("内容：" + webpage.getContent());
+					askStr.append(webpage.getSummary() + "&&");
+				}
+			}
+		} else {
+			conut = searchByPage.size();
+			for (Problem problem : searchByPage) {
+				askStr.append(problem.getAnswer());
+			}
+		}
 		
         if(askStr.toString() != null){
             result.put("code", "0");
             result.put("msg", "搜索成功");
             result.put("result", askStr);
-            result.put("conut", webpages.size());
+            result.put("conut", conut);
         } else {
         	result.put("code", "1");
         	result.put("msg", "没有搜索到结果");
